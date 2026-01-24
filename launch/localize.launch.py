70e47d64953e24dd30eba2dd50261897c5eb2da3
Tranxpotter <michaelyclaw@gmail.com>
2026-01-24 21:49:03 +0800

    clean localize launch

--- launch/localize.launch.py
+++ launch/localize.launch.py
@@ -2,40 +2,56 @@ import os
 import math
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription, TimerAction, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    pkg_name = 'guide_robot_localization'
-    pkg_dir = get_package_share_directory(pkg_name)
+    this_pkg_name = 'guide_robot_localization'
+    this_pkg_dir = get_package_share_directory(this_pkg_name)
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     fast_lio_dir = get_package_share_directory('fast_lio') 
 
-    map_file = os.path.join(pkg_dir, 'maps', 'map.yaml')
-    nav2_config_file = os.path.join(pkg_dir, 'config', 'nav2_params.yaml')
+    default_map_path = os.path.join(this_pkg_dir, 'maps', 'map.yaml')
+    default_nav2_config_path = os.path.join(this_pkg_dir, 'config', 'nav2_params.yaml')
+
+    declare_map_path = DeclareLaunchArgument("map_path", default_value=default_map_path)
+    declare_nav2_config_path = DeclareLaunchArgument("nav2_config_path", default_value=default_nav2_config_path)
 
     # FRAMES
+    map_frame = 'map'
     odom_frame = 'odom'
-    base_frame = 'base_footprint'
-    lidar_frame = 'livox_frame'
+    camera_init_frame = 'camera_init'
+    body_frame = 'body'
+    base_footprint_frame = 'base_footprint'
+    lidar_frame = 'lidar'
     
 
-    # TILT CALCULATION (60 Degrees)
-    angle_deg = 60.0 
+    # Lidar frame tilt transform calculation (60 pitch, 180 yaw) #TODO: what's the actual lidar frame angles?
+    angle_deg = 60.0
     angle_rad = angle_deg * (math.pi / 180.0)
     z_rotation_angle = 180.0
     z_rotation_rad = z_rotation_angle * (math.pi / 180.0)
-    lidar_tf_args = ['0.1', '0', '0.5', '0', str(angle_rad), str(z_rotation_rad), base_frame, lidar_frame]
+    lidar_tf_args = ['0.1', '0', '0.4', str(z_rotation_rad), str(angle_rad), '0', body_frame, lidar_frame] # (x, y, z, yaw, pitch, roll, parent frame, child )
 
+    # Saved lidar tf from body? or base_footprint
+        # extrinsic_T: [ 0.1, 0.0, 0.4 ]
+        # extrinsic_R: [ -0.5, 0.0, 0.866025,
+        #                 0.0, -1.0, 0.0,
+        #                 0.866025, 0.0, 0.5]
+    
+    '''
+        TF TREE (Subject to change): 
+        map             -nav2
+        odom            -nav2
+        camera_init     -fastlio
+        body            -nav2 fastlio
+        lidar base_footprint
+    '''
 
-                # extrinsic_T: [ 0.1, 0.0, 0.4 ]
-            # extrinsic_R: [ -0.5, 0.0, 0.866025,
-            #                 0.0, -1.0, 0.0,
-            #                 0.866025, 0.0, 0.5]
     # --- NODES ---
-
-    # 1. FAST-LIO
+    # 1. Launch FAST-LIO (camera_init -> body)
     fast_lio_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(fast_lio_dir, 'launch', 'mapping.launch.py')
@@ -44,15 +60,15 @@ def generate_launch_description():
     )
 
     # 2. TF GLUE
-    # Connect FAST-LIO's world frame to ROS navigation frames
+    # Connect ROS navigation frames to FAST-LIO's world frame
     tf_odom_glue = Node(
         package='tf2_ros', executable='static_transform_publisher',
-        arguments=['0','0','0','0','0','0', odom_frame, "camera_init"]
+        arguments=['0','0','0','0','0','0', odom_frame, camera_init_frame]
     )
-    # Connect FAST-LIO's body frame to standard robot base frame
+    # Connect FAST-LIO's body frame to robot base footprint
     tf_base_glue = Node(
         package='tf2_ros', executable='static_transform_publisher',
-        arguments=['0','0','0','0','0','0', 'body', base_frame]
+        arguments=['0','0','-0.4','0','0','0', body_frame, base_footprint_frame]
     )
     # LiDAR mounting position transform
     tf_base_lidar = Node(
@@ -62,15 +78,11 @@ def generate_launch_description():
     )
 
 
-    # 4. ROTATE POINT CLOUD TO base_frame
+    # 4. Transform Point Cloud from camera_init to lidar frame
     rotate_pc_node = Node(
         package='guide_robot_localization',
         executable='point_cloud_transformer',
-        name='pointcloud_rotator',
-        remappings=[
-            ('/input_point_cloud', '/Laser_map'),
-            ('/output_point_cloud', '/cloud_rotated')
-        ],
+        name='point_cloud_transformer',
         parameters=[{
             'input_topic': '/Laser_map',
             'output_topic': '/cloud_rotated',
@@ -86,17 +98,17 @@ def generate_launch_description():
         name='pointcloud_to_laserscan',
         remappings=[
             ('cloud_in', '/cloud_rotated'), 
-            ('scan', '/scan')
+            ('scan', '/scan') #Output scan topic
         ],
         parameters=[{
-            'target_frame': base_frame, 
-            'transform_tolerance': 0.1, # Increased tolerance for processing time
+            'target_frame': base_footprint_frame, # Slice the point_cloud based on the transformation of the target_frame
+            'transform_tolerance': 0.1, # Time tolerance for transform lookups
             'min_height': 0.1,
             'max_height': 1.2,
             'angle_min': -3.14159,
             'angle_max': 3.14159,
             'angle_increment': 0.0043,
-            'scan_time': 0.1, #
+            'scan_time': 0.1, # scan rate in seconds
             'range_min': 0.2,
             'range_max': 50.0,
             'use_inf': True
@@ -112,9 +124,9 @@ def generate_launch_description():
                     os.path.join(nav2_bringup_dir, 'launch', 'localization_launch.py')
                 ),
                 launch_arguments={
-                    'map': map_file,
+                    'map': LaunchConfiguration("map_path"),
                     'use_sim_time': 'false',
-                    'params_file': nav2_config_file,
+                    'params_file': LaunchConfiguration("nav2_config_path"),
                     'autostart': 'true'
                 }.items()
             )
@@ -125,10 +137,12 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', os.path.join(pkg_dir, 'rviz', 'config.rviz')]
+        arguments=['-d', os.path.join(this_pkg_dir, 'rviz', 'config.rviz')]
     )
 
     return LaunchDescription([
+        declare_map_path, 
+        declare_nav2_config_path, 
         fast_lio_launch,
         tf_odom_glue,
         tf_base_glue,
